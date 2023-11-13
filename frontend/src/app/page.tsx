133e9f3f5b9dfbc6011d46338ce4929d7203f71a
francisco1code <francisco1code@gmail.com>
2023-11-13 18:04:32 -0300

    Resolve conflit home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
+import { MenuButton } from '@/componets/menu-button'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-        </p>
+      <div className={styles.description}>  
+          <MenuButton/> 
       </div>
     </main>
   )
